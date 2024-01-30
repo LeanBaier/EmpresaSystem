d8a72936050dca5dd3b040ae904d2b82731a8bc0
CrazyPunky <crazypunky@protonmail.com>
2024-01-30 08:52:15 -0300

    Se agregan fragments y componentes vue

--- employee-app/src/main/java/app/system/employee/api/EmployeeApiController.java
+++ employee-app/src/main/java/app/system/employee/api/EmployeeApiController.java
@@ -1,9 +1,14 @@
 package app.system.employee.api;
 
+import app.system.employee.dto.GetEmployeesRequest;
+import app.system.employee.dto.PagedData;
+import app.system.employee.model.EmployeeModel;
 import app.system.employee.service.EmployeeService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,9 +21,16 @@ public class EmployeeApiController {
     private final EmployeeService employeeService;
 
     @DeleteMapping("/{idEmployee}")
-    public ResponseEntity<Void> deleteEmployee(@PathVariable Long idEmployee){
+    public ResponseEntity<Void> deleteEmployee(@PathVariable Long idEmployee) {
         employeeService.deleteEmployee(idEmployee);
         return ResponseEntity.ok().build();
     }
+
+    @GetMapping("/search")
+    public ResponseEntity<PagedData<EmployeeModel>> searchEmployees(@Valid GetEmployeesRequest filters) {
+        var result = employeeService.getEmployees(filters);
+        return ResponseEntity.ok(result);
+    }
+
 
 }
--- employee-app/src/main/java/app/system/employee/controller/EmployeeController.java
+++ employee-app/src/main/java/app/system/employee/controller/EmployeeController.java
@@ -1,14 +1,10 @@
 package app.system.employee.controller;
 
-import app.system.employee.dto.GetEmployeesRequest;
 import app.system.employee.service.EmployeeService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Date;
@@ -23,21 +19,16 @@ public class EmployeeController {
     @GetMapping("/home")
     public String home(Model model) {
         model.addAttribute("serverTime", new Date());
-        return "employees/home";
+        model.addAttribute("fragmentName", "employees/home");
+        model.addAttribute("fragmentTag", "main");
+        return "general";
     }
 
     @GetMapping("/search")
-    public String searchView(@Valid @ModelAttribute("filters")
-                             GetEmployeesRequest filters, BindingResult bindingResult, Model model) {
-        if (bindingResult.hasErrors()) {
-            return "employees/search";
-        }
-        if (model.containsAttribute("filters.size") && !model.getAttribute("filters.size").equals(filters.getSize().toString())) {
-            filters.setSize((Integer) model.getAttribute("filters.size"));
-        }
-        var employeeModelList = employeeService.getEmployees(filters);
-        model.addAttribute("employees", employeeModelList);
-        return "employees/search";
+    public String searchView(Model model) {
+        model.addAttribute("fragmentName", "employees/search");
+        model.addAttribute("fragmentTag", "main");
+        return "general";
     }
 
 }
